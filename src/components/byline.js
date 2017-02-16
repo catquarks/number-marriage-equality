3cef26a8e418044388373a9a9a587dce252dc0bc
Tami <catquarks@gmail.com>
2017-02-16 00:13:34 -0500

    refactor display
    Display component previously manipulated DOM directly; now it does not

--- src/components/byline.js
+++ src/components/byline.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import './css/byline.css'
 
-function Byline(){
+export default function Byline(){
 	return(
 		<div id="byline" className="hidden">
 			<span className="secrets">
 				<p>
-					<small>
-						made by <a href="http://tamarajaton.xyz" target="_blank">tj</a> 2017
-					</small>
+					made by <a href="http://tamarajaton.xyz" target="_blank">tj</a> 2017
 				</p>
 			</span>
 		</div>
 	)
 }
-
-export default Byline
--- src/components/display.js
+++ src/components/display.js
@@ -11,15 +11,22 @@ export default function(props) {
 		}
 
 		if (props.married){
-			document.getElementById("heart").style.fill = "#cc2121"
 			return props.consumation
+		}
+	}
+
+	function colorHeart(){
+		if (!props.married){
+			return "#dd3232"
+		} else {
+			return "#cc2121"
 		}
 	}
 
 	return(
 		<div id="display">
 			<svg viewBox="0 0 32 29.6">
-				<path id='heart' d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2 c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+				<path id='heart' fill={colorHeart()} d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2 c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
 				<text x="50%" y="50%">{drawDisplay()}</text>
 			</svg>
 		</div>
